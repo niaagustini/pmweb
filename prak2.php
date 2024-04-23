628e0c9e68af44f5b00ec5b4e8ecd1048cb266b3
niaagustini <niaagustini1404@gmail.com>
2024-04-23 09:02:57 +0800

    Initial commit

---
+++ prak2.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h2>Nilai Mahasiswa : </h2>";
+$nim = '2301010153';
+$nama = 'Nia Agustini';
+$prodi = 'Sistem Komputer';
+$nilai = 90;
+echo "NIM : ", $nim, "<br>";
+echo "Nama : ", $nama, "<br>";
+echo "Prodi : ", $prodi, "<br>";
+echo "Nilai : $nilai";
+?>
---
+++ prak5.php
@@ -0,0 +1,9 @@
+<?php
+$username = "Nia Agustini";
+if ($username == "Nia Agustini") {
+echo "Anda Berhasil Login!, Selamat datang
+$username";
+} else {
+echo "Username salah, silahkan coba lagi!";
+}
+?>
